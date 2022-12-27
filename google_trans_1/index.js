27039df9dce2e0ffbf0c88e45b18c6585ed87169
Ali Nategh <alinategh005@gmail.com>
2022-12-27 17:18:14 +0330

    test

--- google_trans_1/index.js
+++ google_trans_1/index.js
@@ -1,11 +1,23 @@
-const translate = require('google-translate-api');
+// const translate = require('google-translate-api');
 
 
-translate('Ik spreek Engels', {to: 'en'}).then(res => {
-    console.log(res.text);
-    //=> I speak English
-    console.log(res.from.language.iso);
-    //=> nl
-}).catch(err => {
-    console.error(err);
-});
+// translate('Ik spreek Engels', {to: 'en'}).then(res => {
+//     console.log(res.text);
+//     //=> I speak English
+//     console.log(res.from.language.iso);
+//     //=> nl
+// }).catch(err => {
+//     console.error(err);
+// });
+
+
+
+
+// const translate = require('google-translate-api-x');
+
+// async function tr(sentence) {
+//     let translation = await translate(`${sentence}`, {to: 'fa'});
+//     return translation.text
+// }
+// tr("Hello world")
+// console.log();
